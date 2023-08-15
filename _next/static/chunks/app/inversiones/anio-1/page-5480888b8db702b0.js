2762f3b3a389d7796c01974ece30275f58801ab6
DanielSebasCM <danielsebascm@hotmail.com>
2023-08-15 10:46:13 -0600

    V1

---
+++ _next/static/chunks/app/inversiones/anio-1/page-5480888b8db702b0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[113,692,899,473,632,344,74],{9470:function(n,e,u){Promise.resolve().then(u.t.bind(u,3222,23))}},function(n){n.O(0,[222,971,596,744],function(){return n(n.s=9470)}),_N_E=n.O()}]);
